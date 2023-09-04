87b4c557a6dc35e57df3ca3a676b282f835e530b
Nicolas Abril <nicolas@higherorderco.com>
2023-09-04 17:51:49 +0200

    Add different modes of running the cli

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,40 @@ pub mod to_core;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-  path: PathBuf,
+  #[arg(long, default_value = "true", help = "Just checks if the file ok. Default mode", group = "mode")]
+  pub check: bool,
+
+  #[arg(short, long, group = "mode", help = "Compiles to HVM-core and prints to stdout")]
+  pub compile: bool,
+
+  #[arg(short, long, group = "mode", help = "Runs the hvm-lang file.")]
+  pub run: bool,
+
+  #[arg(short, long)]
+  pub verbose: bool,
+
+  #[arg(help = "Path to the input file")]
+  pub path: PathBuf,
 }
 
 fn main() -> anyhow::Result<()> {
   let args = Args::parse();
+
   let book = loader::load_file_to_book(&args.path)?;
-  println!("{book:?}");
+
+  if args.verbose {
+    println!("{book:?}");
+  }
+
   let compiled = to_core::book_to_hvm_core(&book)?;
-  println!("{compiled}");
+
+  if args.compile {
+    println!("{compiled}");
+  }
+
+  if args.run {
+    todo!()
+  }
+
   Ok(())
 }
