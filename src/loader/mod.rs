67616abf1166accaab8abfdb8f51cfe16090f36b
imaqtkatt <eduarda.so@proton.me>
2023-10-09 18:33:12 -0300

    Fix wrong error outputs by using miette

--- src/loader/mod.rs
+++ src/loader/mod.rs
@@ -2,6 +2,7 @@ use crate::{ast::DefinitionBook, parser::parse_definition_book};
 use ariadne::{Color, Label, Report, ReportKind, Source};
 use chumsky::prelude::Rich;
 use itertools::Itertools;
+use miette::{miette, Diagnostic, NamedSource, SourceSpan};
 use std::{fmt::Display, ops::Range, path::Path};
 
 /// Reads a file and parses to a definition book.
@@ -10,7 +11,7 @@ pub fn load_file_to_book(path: &Path) -> anyhow::Result<DefinitionBook> {
   match parse_definition_book(&code) {
     Ok(book) => Ok(book),
     Err(errs) => {
-      let msg = errs.into_iter().map(|e| display_err_for_console(e, path, &code)).join("\n");
+      let msg = errs.into_iter().map(|e| display_miette_err_for_console(e, path, &code)).join("\n");
       Err(anyhow::anyhow!(msg))
     }
   }
@@ -37,4 +38,47 @@ pub fn err_to_report<'a, T: Display>(err: Rich<T>, path: &'a str) -> Report<'a, 
         .with_color(Color::Red),
     )
     .finish()
+}
+
+/// Displays a [SyntaxError] from the given `err`.
+pub fn display_miette_err_for_console<T: Display>(err: Rich<T>, path: &Path, code: &str) -> String {
+  let source = code.to_string();
+  let name = path.to_str().unwrap();
+
+  let src = NamedSource::new(name, source);
+  let error = SyntaxError::from_rich(err, src);
+
+  let report = miette!(error);
+
+  println!("{report:?}");
+
+  report.to_string()
+}
+
+#[derive(thiserror::Error, Debug, Diagnostic)]
+#[error("{}", error)]
+#[diagnostic()]
+/// This structure holds information for syntax errors.
+struct SyntaxError {
+  /// The error name.
+  error: String,
+  #[source_code]
+  /// The file name and source.
+  src: NamedSource,
+  #[label("{}", reason)]
+  /// The error byte range.
+  span: SourceSpan,
+  /// The error reason.
+  reason: String,
+}
+
+impl SyntaxError {
+  /// Creates a new [SyntaxError] from a [Rich<T>] `err`.
+  fn from_rich<T: Display>(err: Rich<T>, src: NamedSource) -> Self {
+    let error = err.to_string();
+    let reason = err.reason().to_string();
+    let span = SourceSpan::from(err.span().into_range());
+
+    Self { error, reason, span, src }
+  }
 }
