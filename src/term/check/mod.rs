6c7cd46d43bf7dbef06b7f5c1d1b578e3e9f2221
imaqtkatt <eduarda.so@proton.me>
2023-10-16 14:16:18 -0300

    Refactor to use values method

--- src/term/check/mod.rs
+++ src/term/check/mod.rs
@@ -13,7 +13,7 @@ impl DefinitionBook {
 
   /// Check that a definition is not just a reference to itself
   pub fn check_ref_to_ref(&self) -> anyhow::Result<()> {
-    for (_, def) in &self.defs {
+    for def in self.defs.values() {
       if let Term::Ref { .. } = def.body {
         return Err(anyhow::anyhow!(
           "Definition {} is just a reference to another definition",
