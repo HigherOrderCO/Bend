29f2d717d48ada548a3a27678de0103b8a290f05
Nicolas Abril <nicolas.abril@protonmail.ch>
2023-11-10 20:02:48 +0100

    [sc-239] Fix 0-ary patterns after parse, simplify exhaustiveness check

---
+++ src/term/transform/resolve_ctrs_in_pats.rs
@@ -0,0 +1,21 @@
+use crate::term::{Book, RulePat};
+
+impl Book {
+  /// Resolve Constructor names inside rule patterns.
+  /// When parsing a rule we don't have all the constructors yet,
+  /// so no way to know if a particular name belongs to a constructor or is a matched variable.
+  /// Therefore we must do it later, here.
+  pub fn resolve_ctrs_in_pats(&mut self) {
+    for def in self.defs.values_mut() {
+      for rule in &mut def.rules {
+        for pat in &mut rule.pats {
+          if let RulePat::Var(nam) = &pat {
+            if self.ctrs.contains_key(nam) {
+              *pat = RulePat::Ctr(nam.clone(), vec![])
+            }
+          }
+        }
+      }
+    }
+  }
+}
