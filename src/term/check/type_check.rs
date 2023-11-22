a2cd95f3469e93fb704b5476977723d1f730ba30
LunaAmora <luna.mberry@gmail.com>
2023-11-22 12:06:21 -0400

    Update typecheck.rs

--- src/term/check/type_check.rs
+++ src/term/check/type_check.rs
@@ -1,7 +1,6 @@
+use crate::term::{Book, DefId, Definition, Name, RulePat};
 use core::fmt;
 use std::collections::HashMap;
-
-use crate::term::{Book, DefId, Name, RulePat};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Type {
@@ -16,46 +15,56 @@ impl Book {
   /// and checks the rules arities based on the first rule arity.
   pub fn infer_def_types(&self) -> Result<DefinitionTypes, String> {
     let mut def_types = HashMap::new();
-    for def in self.defs.values() {
-      self.infer_def_type(def, &mut def_types)?;
+    for (def_id, def) in &self.defs {
+      let def_type = def.infer_type(&self.ctrs)?;
+      def_types.insert(*def_id, def_type);
     }
     Ok(def_types)
   }
+}
 
-  fn infer_def_type(
-    &self,
-    def: &crate::term::Definition,
-    def_types: &mut HashMap<DefId, Vec<Type>>,
-  ) -> Result<(), String> {
-    let current_arity = def.arity();
-    let mut arg_types = vec![Type::Any; current_arity];
-    for rule in def.rules.iter() {
-      if rule.arity() != current_arity {
-        return Err("Arity error.".to_string());
-      }
-      for (idx, pat) in rule.pats.iter().enumerate() {
-        if let RulePat::Ctr(nam, _) = pat {
-          match self.ctrs.get(nam) {
-            Some(nam) => {
-              let t = Type::Adt(nam.clone());
-              unify(t, idx, &mut arg_types)?;
-            }
-            None => return Err("Unknown constructor '{nam}'.".to_string()),
-          }
-        }
-      }
+impl Definition {
+  pub fn infer_type(&self, ctrs: &HashMap<Name, Name>) -> Result<Vec<Type>, String> {
+    let mut arg_types = vec![];
+
+    for arg_idx in 0 .. self.arity() {
+      let pats = self.rules.iter().map(|r| &r.pats[arg_idx]);
+      arg_types.push(infer_arg_type(pats, ctrs)?);
     }
-    def_types.insert(def.def_id, arg_types);
-    Ok(())
+    Ok(arg_types)
   }
 }
 
-fn unify(t: Type, idx: usize, ctx: &mut [Type]) -> Result<(), String> {
-  if ctx[idx] == Type::Any {
-    ctx[idx] = t;
-  } else if ctx[idx] != t.clone() {
-    return Err(format!("Type mismatch. Found '{}' expected {}.", t, ctx[idx]));
+fn infer_arg_type<'a>(
+  pats: impl Iterator<Item = &'a RulePat>,
+  ctrs: &HashMap<Name, Name>,
+) -> Result<Type, String> {
+  let mut arg_type = Type::Any;
+  for pat in pats {
+    let pat_type = match pat {
+      RulePat::Var(_) => Type::Any,
+      RulePat::Ctr(ctr_nam, _) => {
+        if let Some(adt_nam) = ctrs.get(ctr_nam) {
+          Type::Adt(adt_nam.clone())
+        } else {
+          return Err("Unknown constructor '{nam}'.".to_string());
+        }
+      }
+      RulePat::Num(_) => todo!(),
+    };
+    unify(pat_type, &mut arg_type)?;
   }
+  Ok(arg_type)
+}
+
+fn unify(new: Type, old: &mut Type) -> Result<(), String> {
+  match (new, &old) {
+    (Type::Adt(new), Type::Any) => *old = Type::Adt(new),
+    (Type::Adt(new), Type::Adt(old)) if &new != old => {
+      return Err(format!("Type mismatch. Found '{}' expected {}.", new, old));
+    }
+    _ => (),
+  };
   Ok(())
 }
 
@@ -65,5 +74,26 @@ impl fmt::Display for Type {
       Type::Any => write!(f, "any"),
       Type::Adt(nam) => write!(f, "{nam}"),
     }
+  }
+}
+
+impl Book {
+  pub fn check_arity(&self) -> Result<(), String> {
+    for def in self.defs.values() {
+      def.check_arity()?;
+    }
+    Ok(())
+  }
+}
+
+impl Definition {
+  pub fn check_arity(&self) -> Result<(), String> {
+    let expected_arity = self.arity();
+    for rule in &self.rules {
+      if rule.arity() != expected_arity {
+        return Err("Arity error.".to_string());
+      }
+    }
+    Ok(())
   }
 }
