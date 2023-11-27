84d842acae2162622e4c2d0d288a7bebf7a17478
LunaAmora <luna.mberry@gmail.com>
2023-11-27 16:57:37 -0400

    Fix linearize_matches functions

--- src/term/transform/linearize_matches.rs
+++ src/term/transform/linearize_matches.rs
@@ -18,8 +18,9 @@ impl Term {
     match self {
       Term::Lam { nam, bod } => {
         bod.linearize_matches();
-        linearize_matches(bod, nam.as_ref())
+        bod.linearize_matches_by(nam.as_ref())
       }
+
       Term::Chn { bod, .. } => bod.linearize_matches(),
 
       Term::Match { scrutinee, arms } => {
@@ -42,59 +43,59 @@ impl Term {
       Term::Var { .. } | Term::Ref { .. } | Term::Num { .. } | Term::Lnk { .. } | Term::Era => {}
     }
   }
-}
 
-fn linearize_matches(term: &mut Term, name: Option<&Name>) {
-  match term {
-    Term::Lam { bod, .. } | Term::Chn { bod, .. } => linearize_matches(bod, name),
+  fn linearize_matches_by(&mut self, name: Option<&Name>) {
+    match self {
+      Term::Lam { bod, .. } | Term::Chn { bod, .. } => bod.linearize_matches_by(name),
 
-    Term::Let { pat: LetPat::Var(nam), val, nxt } => {
-      linearize_matches(val, name);
-      if !name.is_some_and(|n| n == nam) {
-        linearize_matches(nxt, name);
-      }
-    }
-
-    Term::Let { pat: LetPat::Tup(fst, snd), val, nxt } | Term::Dup { fst, snd, val, nxt, .. } => {
-      linearize_matches(val, name);
-      if !name
-        .is_some_and(|nam| fst.as_ref().is_some_and(|n| n == nam) || snd.as_ref().is_some_and(|n| n == nam))
-      {
-        linearize_matches(nxt, name);
-      }
-    }
-
-    Term::App { fun: fst, arg: snd }
-    | Term::Tup { fst, snd }
-    | Term::Sup { fst, snd }
-    | Term::Opx { fst, snd, .. } => {
-      linearize_matches(fst, name);
-      linearize_matches(snd, name);
-    }
-
-    Term::Match { scrutinee, .. } => {
-      let Term::Var { nam } = &**scrutinee else { unreachable!() };
-
-      if name.is_some_and(|n| n == nam) {
-        return;
+      Term::Let { pat: LetPat::Var(nam), val, nxt } => {
+        val.linearize_matches_by(name);
+        if !name.is_some_and(|n| n == nam) {
+          nxt.linearize_matches_by(name);
+        }
       }
 
-      let Term::Match { scrutinee, arms } = std::mem::take(term) else { unreachable!() };
+      Term::Let { pat: LetPat::Tup(fst, snd), val, nxt } | Term::Dup { fst, snd, val, nxt, .. } => {
+        val.linearize_matches_by(name);
+        if !name
+          .is_some_and(|nam| fst.as_ref().is_some_and(|n| n == nam) || snd.as_ref().is_some_and(|n| n == nam))
+        {
+          nxt.linearize_matches_by(name);
+        }
+      }
 
-      let arms = arms
-        .into_iter()
-        .map(|(rule, arm)| {
-          let arm = Term::Lam { nam: name.cloned(), bod: Box::new(arm) };
-          (rule, arm)
-        })
-        .collect();
+      Term::App { fun: fst, arg: snd }
+      | Term::Tup { fst, snd }
+      | Term::Sup { fst, snd }
+      | Term::Opx { fst, snd, .. } => {
+        fst.linearize_matches_by(name);
+        snd.linearize_matches_by(name);
+      }
 
-      *term = Term::App {
-        fun: Box::new(Term::Match { scrutinee, arms }),
-        arg: Box::new(Term::Var { nam: name.cloned().unwrap_or(Name::new("*")) }),
-      };
+      Term::Match { scrutinee, .. } => {
+        let Term::Var { nam } = &**scrutinee else { unreachable!() };
+
+        if name.is_some_and(|n| n == nam) {
+          return;
+        }
+
+        let Term::Match { scrutinee, arms } = std::mem::take(self) else { unreachable!() };
+
+        let arms = arms
+          .into_iter()
+          .map(|(rule, arm)| {
+            let arm = Term::Lam { nam: name.cloned(), bod: Box::new(arm) };
+            (rule, arm)
+          })
+          .collect();
+
+        *self = Term::App {
+          fun: Box::new(Term::Match { scrutinee, arms }),
+          arg: Box::new(name.cloned().map(|nam| Term::Var { nam }).unwrap_or_default()),
+        };
+      }
+
+      Term::Var { .. } | Term::Ref { .. } | Term::Num { .. } | Term::Lnk { .. } | Term::Era => {}
     }
-
-    Term::Var { .. } | Term::Ref { .. } | Term::Num { .. } | Term::Lnk { .. } | Term::Era => {}
   }
 }
