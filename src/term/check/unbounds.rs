64ed08da6cdff4e1cfb9123bc33af1c6fb3b043f
imaqtkatt <eduarda.so@proton.me>
2024-01-26 17:57:00 -0300

    Remove wrong unused file

--- src/term/check/unbounds.rs
+++
@@ -1,9 +0,0 @@
-use crate::term::Book;
-
-impl Book {
-  // Checks for both unbound patterns and unbound variables.
-  pub fn check_unbounds(&self) -> Result<(), String> {
-    self.check_unbound_vars()?;
-    self.check_unbound_pats()
-  }
-}
