6e93dbc3169b3ea08e555ebcbfdebeb18206893e
imaqtkatt <eduarda.so@proton.me>
2024-01-31 13:50:10 -0300

    Add location and highlight on parser errors

--- src/term/load_book.rs
+++ src/term/load_book.rs
@@ -8,12 +8,12 @@ use std::path::Path;
 /// Reads a file and parses to a definition book.
 pub fn load_file_to_book(path: &Path) -> Result<Book, String> {
   let code = std::fs::read_to_string(path).map_err(|e| e.to_string())?;
-  do_parse_book(&code)
+  do_parse_book(&code, path)
 }
 
-pub fn do_parse_book(code: &str) -> Result<Book, String> {
+pub fn do_parse_book(code: &str, path: &Path) -> Result<Book, String> {
   match parse_definition_book(code, Book::builtins, Origin::User) {
     Ok(book) => Ok(book),
-    Err(errs) => Err(errs.iter().map(|e| error_to_msg(e, code)).join("\n")),
+    Err(errs) => Err(errs.iter().map(|e| error_to_msg(e, code, path)).join("\n")),
   }
 }
