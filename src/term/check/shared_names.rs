a179e1042fc5d5fe3dd5399344b61e96dcb39a2d
imaqtkatt <eduarda.so@proton.me>
2024-03-04 09:05:32 -0300

    Allow constructors share names with data types

--- src/term/check/shared_names.rs
+++ src/term/check/shared_names.rs
@@ -2,7 +2,10 @@ use std::fmt::Display;
 
 use indexmap::IndexMap;
 
-use crate::term::{Ctx, Name};
+use crate::{
+  diagnostics,
+  term::{Ctx, Name},
+};
 
 #[derive(Debug, Clone)]
 pub struct TopLevelErr(Name);
@@ -14,26 +17,70 @@ impl Display for TopLevelErr {
 }
 
 impl Ctx<'_> {
-  /// Checks if exists shared names from definitions, adts and constructors.
   pub fn check_shared_names(&mut self) {
-    let mut checked = IndexMap::<&Name, usize>::new();
+    let mut checked = IndexMap::<&Name, NameInfo>::new();
 
     for adt_name in self.book.adts.keys() {
-      *checked.entry(adt_name).or_default() += 1;
+      checked.entry(adt_name).or_insert(NameInfo::new(NameKind::Adt)).with_adt(adt_name, &mut self.info);
     }
 
     for ctr_name in self.book.ctrs.keys() {
-      *checked.entry(ctr_name).or_default() += 1;
+      checked.entry(ctr_name).or_insert(NameInfo::new(NameKind::Ctr)).with_ctr(ctr_name, &mut self.info);
     }
 
     for def_name in self.book.defs.keys() {
-      *checked.entry(def_name).or_default() += 1;
+      checked.entry(def_name).or_insert(NameInfo::new(NameKind::Def)).with_def(def_name, &mut self.info);
     }
 
-    for (name, n) in checked.into_iter() {
-      if n > 1 {
+    for (name, name_info) in checked.into_iter() {
+      if name_info.count > 1 {
         self.info.error(TopLevelErr(name.clone()));
       }
+    }
+  }
+}
+
+#[derive(Debug)]
+enum NameKind {
+  Adt,
+  Def,
+  Ctr,
+}
+
+#[derive(Debug)]
+struct NameInfo {
+  kind: NameKind,
+  count: usize,
+}
+
+impl NameInfo {
+  fn new(kind: NameKind) -> Self {
+    Self { kind, count: 0 }
+  }
+}
+
+impl NameInfo {
+  fn with_ctr(&mut self, current_name: &Name, info: &mut diagnostics::Info) {
+    match self.kind {
+      NameKind::Adt => {} // Catch by the parser
+      NameKind::Def => info.error(TopLevelErr(current_name.clone())),
+      NameKind::Ctr => {} // Catch by the parser
+    }
+  }
+
+  fn with_def(&mut self, current_name: &Name, info: &mut diagnostics::Info) {
+    match self.kind {
+      NameKind::Adt => self.count += 1,
+      NameKind::Def => {}
+      NameKind::Ctr => info.error(TopLevelErr(current_name.clone())),
+    }
+  }
+
+  fn with_adt(&mut self, current_name: &Name, info: &mut diagnostics::Info) {
+    match self.kind {
+      NameKind::Adt => self.count += 1,
+      NameKind::Def => info.error(TopLevelErr(current_name.clone())),
+      NameKind::Ctr => self.count += 1,
     }
   }
 }
