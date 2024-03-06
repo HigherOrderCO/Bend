b29ced1bb219d0ee3fe20172578d45d33a82510a
imaqtkatt <eduarda.so@proton.me>
2024-03-06 11:54:24 -0300

    Include cli tests stderr output

--- tests/golden_tests.rs
+++ tests/golden_tests.rs
@@ -73,7 +73,7 @@ fn run_golden_test_dir_multiple(test_name: &str, run: &[&dyn Fn(&str, &Path) -> 
 
   let walker = WalkDir::new(&root).sort_by_file_name().max_depth(2).into_iter().filter_entry(|e| {
     let path = e.path();
-    path == root || (path.is_file() && path.extension().is_some_and(|x| x == "hvm"))
+    path == root || path.is_dir() || (path.is_file() && path.extension().is_some_and(|x| x == "hvm"))
   });
 
   for entry in walker {
@@ -318,8 +318,9 @@ fn cli() {
     args_file.read_to_string(&mut args_buf).expect("Read args");
     let args = args_buf.lines();
 
-    let output = std::process::Command::new("cargo").arg("run").args(args).output().expect("Run process");
+    let output =
+      std::process::Command::new("cargo").arg("run").arg("-q").args(args).output().expect("Run process");
 
-    Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
+    Ok(format!("{}\n{}", String::from_utf8_lossy(&output.stderr), String::from_utf8_lossy(&output.stdout)))
   })
 }
