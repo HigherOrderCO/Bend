71308b36c4cbf733e461b99eccf22ae46e22d4e3
imaqtkatt <eduarda.so@proton.me>
2024-04-29 11:12:11 -0300

    Fix assign pattern to lang

--- src/term/flavour_py/to_lang.rs
+++ src/term/flavour_py/to_lang.rs
@@ -1,9 +1,9 @@
-use crate::term;
+use crate::term as lang;
 
 use super::{AssignPattern, Definition, Program, Stmt, Term};
 
 impl Program {
-  pub fn to_lang(self, mut book: term::Book) -> term::Book {
+  pub fn to_lang(self, mut book: lang::Book) -> lang::Book {
     for (def_name, def) in self.defs {
       book.defs.insert(def_name, def.to_lang());
     }
@@ -13,7 +13,7 @@ impl Program {
         book.ctrs.insert(variant.0.clone(), enum_name.clone());
       }
       let ctrs = r#enum.variants.into_iter().map(|(k, v)| (k, v.fields)).collect();
-      let adt = term::Adt { ctrs, builtin: false };
+      let adt = lang::Adt { ctrs, builtin: false };
       book.adts.insert(enum_name, adt);
     }
 
@@ -22,41 +22,41 @@ impl Program {
 }
 
 impl Definition {
-  pub fn to_lang(self) -> term::Definition {
-    let rule = term::Rule {
-      pats: self.params.into_iter().map(|param| term::Pattern::Var(Some(param))).collect(),
+  pub fn to_lang(self) -> lang::Definition {
+    let rule = lang::Rule {
+      pats: self.params.into_iter().map(|param| lang::Pattern::Var(Some(param))).collect(),
       body: self.body.to_lang(),
     };
 
-    term::Definition { name: self.name, rules: vec![rule], builtin: false }
+    lang::Definition { name: self.name, rules: vec![rule], builtin: false }
   }
 }
 
 impl AssignPattern {
-  pub fn to_lang(self) -> term::Pattern {
+  pub fn to_lang(self) -> lang::Pattern {
     match self {
-      AssignPattern::Var(name) => term::Pattern::Var(Some(name)),
-      AssignPattern::Tup(names) => term::Pattern::Fan(
-        term::FanKind::Dup,
-        term::Tag::Static,
-        names.into_iter().map(|name| term::Pattern::Var(Some(name))).collect(),
+      AssignPattern::Var(name) => lang::Pattern::Var(Some(name)),
+      AssignPattern::Tup(names) => lang::Pattern::Fan(
+        lang::FanKind::Tup,
+        lang::Tag::Static,
+        names.into_iter().map(|name| lang::Pattern::Var(Some(name))).collect(),
       ),
     }
   }
 }
 
 impl Stmt {
-  pub fn to_lang(self) -> term::Term {
+  pub fn to_lang(self) -> lang::Term {
     match self {
       Stmt::Assign { pat, val, nxt } => {
         let pat = pat.to_lang();
         let val = val.to_lang();
         let nxt = nxt.to_lang();
-        term::Term::Let { pat: Box::new(pat), val: Box::new(val), nxt: Box::new(nxt) }
+        lang::Term::Let { pat: Box::new(pat), val: Box::new(val), nxt: Box::new(nxt) }
       }
       Stmt::If { cond, then, otherwise } => {
         let arms = vec![otherwise.to_lang(), then.to_lang()];
-        term::Term::Swt { arg: Box::new(cond.to_lang()), bnd: None, with: Vec::new(), pred: None, arms }
+        lang::Term::Swt { arg: Box::new(cond.to_lang()), bnd: None, with: Vec::new(), pred: None, arms }
       }
       Stmt::Match { arg, bind, arms: old_arms } => {
         let arg = arg.to_lang();
@@ -64,7 +64,7 @@ impl Stmt {
         for arm in old_arms {
           arms.push((arm.lft, Vec::new(), arm.rgt.to_lang()))
         }
-        term::Term::Mat { arg: Box::new(arg), bnd: bind, with: Vec::new(), arms }
+        lang::Term::Mat { arg: Box::new(arg), bnd: bind, with: Vec::new(), arms }
       }
       Stmt::Return { term } => term.to_lang(),
     }
@@ -72,31 +72,31 @@ impl Stmt {
 }
 
 impl Term {
-  pub fn to_lang(self) -> term::Term {
+  pub fn to_lang(self) -> lang::Term {
     match self {
-      Term::None => term::Term::Era,
-      Term::Var { nam } => term::Term::Var { nam },
-      Term::Num { val } => term::Term::Num { typ: term::NumType::U24, val },
+      Term::None => lang::Term::Era,
+      Term::Var { nam } => lang::Term::Var { nam },
+      Term::Num { val } => lang::Term::Num { typ: lang::NumType::U24, val },
       Term::Call { fun, args } => {
         let args = args.into_iter().map(Self::to_lang);
-        term::Term::call(fun.to_lang(), args)
+        lang::Term::call(fun.to_lang(), args)
       }
       Term::Lam { pat, bod } => {
-        term::Term::Lam { tag: term::Tag::Static, pat: Box::new(pat.to_lang()), bod: Box::new(bod.to_lang()) }
+        lang::Term::Lam { tag: lang::Tag::Static, pat: Box::new(pat.to_lang()), bod: Box::new(bod.to_lang()) }
       }
       Term::Enum { nam, fields } => {
-        let fun = term::Term::Var { nam };
+        let fun = lang::Term::Var { nam };
         let args = fields.into_iter().map(|(_, arg)| arg.to_lang()).collect::<Vec<_>>();
-        term::Term::call(fun, args)
+        lang::Term::call(fun, args)
       }
       Term::Bin { op, lhs, rhs } => {
-        term::Term::Opr { opr: op, fst: Box::new(lhs.to_lang()), snd: Box::new(rhs.to_lang()) }
+        lang::Term::Opr { opr: op, fst: Box::new(lhs.to_lang()), snd: Box::new(rhs.to_lang()) }
       }
-      Term::Str { val } => term::Term::Str { val },
-      Term::Lst { els } => term::Term::Lst { els: els.into_iter().map(Self::to_lang).collect() },
-      Term::Tup { els } => term::Term::Fan {
-        fan: term::FanKind::Tup,
-        tag: term::Tag::Static,
+      Term::Str { val } => lang::Term::Str { val },
+      Term::Lst { els } => lang::Term::Lst { els: els.into_iter().map(Self::to_lang).collect() },
+      Term::Tup { els } => lang::Term::Fan {
+        fan: lang::FanKind::Tup,
+        tag: lang::Tag::Static,
         els: els.into_iter().map(Self::to_lang).collect(),
       },
     }
