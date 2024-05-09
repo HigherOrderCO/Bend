5af0459070414080caf7d368f492f83e0ec80c48
imaqtkatt <eduarda.so@proton.me>
2024-05-09 13:14:06 -0300

    ...

--- src/imp/gen_map_get.rs
+++ src/imp/gen_map_get.rs
@@ -17,49 +17,49 @@ impl Stmt {
     match self {
       Stmt::Assign { val, nxt, .. } => {
         nxt.gen_map_get();
-        let substs = val.subst_map_gets();
-        if !substs.is_empty() {
-          *self = gen_get(self.clone(), substs);
+        let substitutions = val.substitute_map_gets();
+        if !substitutions.is_empty() {
+          *self = gen_get(self.clone(), substitutions);
         }
       }
       Stmt::InPlace { val, nxt, .. } => {
         nxt.gen_map_get();
-        let substs = val.subst_map_gets();
-        if !substs.is_empty() {
-          *self = gen_get(self.clone(), substs);
+        let substitutions = val.substitute_map_gets();
+        if !substitutions.is_empty() {
+          *self = gen_get(self.clone(), substitutions);
         }
       }
       Stmt::If { cond, then, otherwise } => {
         then.gen_map_get();
         otherwise.gen_map_get();
-        let substs = cond.subst_map_gets();
-        if !substs.is_empty() {
-          *self = gen_get(self.clone(), substs);
+        let substitutions = cond.substitute_map_gets();
+        if !substitutions.is_empty() {
+          *self = gen_get(self.clone(), substitutions);
         }
       }
       Stmt::Match { arg, arms, .. } | Stmt::Fold { arg, arms, .. } => {
         for arm in arms.iter_mut() {
           arm.rgt.gen_map_get();
         }
-        let substs = arg.subst_map_gets();
-        if !substs.is_empty() {
-          *self = gen_get(self.clone(), substs);
+        let substitutions = arg.substitute_map_gets();
+        if !substitutions.is_empty() {
+          *self = gen_get(self.clone(), substitutions);
         }
       }
       Stmt::Switch { arg, arms, .. } => {
         for arm in arms.iter_mut() {
           arm.gen_map_get();
         }
-        let substs = arg.subst_map_gets();
-        if !substs.is_empty() {
-          *self = gen_get(self.clone(), substs);
+        let substitutions = arg.substitute_map_gets();
+        if !substitutions.is_empty() {
+          *self = gen_get(self.clone(), substitutions);
         }
       }
       Stmt::Do { fun: _, block: _ } => todo!(),
       Stmt::Return { term } => {
-        let substs = term.subst_map_gets();
-        if !substs.is_empty() {
-          *self = gen_get(self.clone(), substs);
+        let substitutions = term.substitute_map_gets();
+        if !substitutions.is_empty() {
+          *self = gen_get(self.clone(), substitutions);
         }
       }
     }
@@ -67,58 +67,58 @@ impl Stmt {
 }
 
 impl Expression {
-  fn subst_map_gets(&mut self) -> HashMap<Name, (Name, MapKey)> {
-    fn go(e: &mut Expression, subst: &mut HashMap<Name, (Name, MapKey)>) {
+  fn substitute_map_gets(&mut self) -> HashMap<Name, (Name, MapKey)> {
+    fn go(e: &mut Expression, substitutions: &mut HashMap<Name, (Name, MapKey)>) {
       match e {
         Expression::MapGet { nam, key } => {
-          let new_var = gen_map_var(subst);
-          subst.insert(new_var.clone(), (nam.clone(), *key));
+          let new_var = gen_map_var(substitutions);
+          substitutions.insert(new_var.clone(), (nam.clone(), *key));
           *e = Expression::Var { nam: new_var };
         }
         Expression::Call { fun, args, kwargs } => {
-          go(fun, subst);
+          go(fun, substitutions);
           for arg in args {
-            go(arg, subst);
+            go(arg, substitutions);
           }
           for (_, arg) in kwargs {
-            go(arg, subst);
+            go(arg, substitutions);
           }
         }
         Expression::Lam { bod, .. } => {
-          go(bod, subst);
+          go(bod, substitutions);
         }
         Expression::Bin { lhs, rhs, .. } => {
-          go(lhs, subst);
-          go(rhs, subst);
+          go(lhs, substitutions);
+          go(rhs, substitutions);
         }
         Expression::Lst { els } | Expression::Tup { els } => {
           for el in els {
-            go(el, subst);
+            go(el, substitutions);
           }
         }
         Expression::Comprehension { term, iter, cond, .. } => {
-          go(term, subst);
-          go(iter, subst);
+          go(term, substitutions);
+          go(iter, substitutions);
           if let Some(cond) = cond {
-            go(cond, subst);
+            go(cond, substitutions);
           }
         }
         Expression::MapInit { entries } => {
           for (_, entry) in entries {
-            go(entry, subst);
+            go(entry, substitutions);
           }
         }
         Expression::None | Expression::Str { .. } | Expression::Var { .. } | Expression::Num { .. } => {}
       }
     }
-    let mut subst = HashMap::new();
-    go(self, &mut subst);
-    subst
+    let mut substitutions = HashMap::new();
+    go(self, &mut substitutions);
+    substitutions
   }
 }
 
-fn gen_get(curr: Stmt, substs: HashMap<Name, (Name, MapKey)>) -> Stmt {
-  substs.into_iter().fold(curr, |acc, next| {
+fn gen_get(current: Stmt, substitutions: HashMap<Name, (Name, MapKey)>) -> Stmt {
+  substitutions.into_iter().fold(current, |acc, next| {
     let (var, (map_var, key)) = next;
     let map_get_call = Expression::Var { nam: Name::new("Map/get") };
     let map_get_call = Expression::Call {
@@ -132,6 +132,6 @@ fn gen_get(curr: Stmt, substs: HashMap<Name, (Name, MapKey)>) -> Stmt {
   })
 }
 
-fn gen_map_var(subst: &HashMap<Name, (Name, MapKey)>) -> Name {
-  Name::new(format!("map/get%{}", subst.len()))
+fn gen_map_var(substitutions: &HashMap<Name, (Name, MapKey)>) -> Name {
+  Name::new(format!("map/get%{}", substitutions.len()))
 }
